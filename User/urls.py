aaf463a1115a4ee2114d61d4ddda011aeeb856f8
Sandhe Bhanu prakash <bhanumahesh955@gmail.com>
2024-05-15 14:22:07 +0530

    first commit

---
+++ User/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path 
+
+from .import views 
+
+
+urlpatterns=[ 
+    path('',views.home,name='home'),
+    path('register',views.register,name='register'),
+    path('login',views.login,name='login'),
+    path('logout',views.logout,name='logout'),
+    path('fertilizer',views.fertilizer,name='fertilizer'),
+    path('predict',views.fertilizer,name='fertilizer'),
+    path('crop',views.crop,name='crop'),
+    path('crop_predict',views.crop_predict,name='crop_predict'),
+]
